e0949385574d8a3ea3435b08ade9e23a11d829fa
Danny van Bruggen <hexagonaal@gmail.com>
2024-10-26 01:05:18 +0200

    put the constructor in a factory method

--- src/test/java/com/laamella/snippets_test_junit5/snipsert/SnipsertTest.java
+++ src/test/java/com/laamella/snippets_test_junit5/snipsert/SnipsertTest.java
@@ -4,28 +4,34 @@ import com.laamella.snippets_test_junit5.BasePath;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.RegisterExtension;
 
+import static com.laamella.snippets_test_junit5.snipsert.Snipsert.snipsert;
+
 class SnipsertTest {
     private static final BasePath basePath = BasePath.fromMavenModuleRoot(SnipsertTest.class).inSrcTestResources();
 
     @RegisterExtension
-    static SnipsertExtension snipsertExtension = new SnipsertExtension(
-            basePath.inClassPackageAndNameSubDirectory(SnipsertTest.class),
-            "txt",
-            "\n---\n",
-            ac -> ac);
+    static SnipsertExtension snipsertExtension = createSnipsertExtension(SnipsertTest.class);
+
+    private static SnipsertExtension createSnipsertExtension(Class<?> testClass) {
+        return new SnipsertExtension(
+                basePath.inClassPackageAndNameSubDirectory(testClass),
+                "txt",
+                "\n---\n",
+                ac -> ac);
+    }
 
     @Test
     void abc() {
-        Snipsert.snipsert("Henkie");
+        snipsert("Henkie");
     }
 
     @Test
     void def() {
-        Snipsert.snipsert("Klaas");
+        snipsert("Klaas");
     }
 
     @Test
     void ghi() {
-        Snipsert.snipsert("Pietje");
+        snipsert("Pietje");
     }
 }
